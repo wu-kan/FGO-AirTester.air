b3e4ce1de032c652c9f211a1ba9f7d510f872d06
wu-kan <wu.kan@qq.com>
2020-01-22 00:41:31 +0800

    上传抽奖池jio本

--- FGO-AirTester.py
+++ FGO-AirTester.py
@@ -2,72 +2,14 @@
 __author__ = "wukan"
 
 from airtest.core.api import *
+
 auto_setup(__file__)
-for _iter in range(4):
-    touch(wait(Template(r"tpl1576848245799.png", record_pos=(0.12, -0.069), resolution=(2520, 1080)),timeout=60))
-    sleep()
 
-    v=exists(Template(r"tpl1576851873721.png", record_pos=(-0.093, -0.02), resolution=(2520, 1080)))
-    if v != False:
-        touch(v)
-        touch(wait(Template(r"tpl1576851988180.png", record_pos=(0.064, 0.12), resolution=(2520, 1080)))) # 恰苹果
-
-    touch(wait(Template(r"tpl1576848308190.png", record_pos=(0.242, -0.027), resolution=(2520, 1080))))
-    touch(wait(Template(r"tpl1576848397065.png", record_pos=(0.278, 0.19), resolution=(2520, 1080))))
-
-    wait(Template(r"tpl1576850638587.png", record_pos=(0.242, 0.142), resolution=(2520, 1080)),timeout=60) # 等待攻击界面加载完成
-
-    touch(v=(400,900)) # 艾蕾2技能
-    touch(wait(Template(r"tpl1576850738968.png", record_pos=(0.075, 0.038), resolution=(2520, 1080))))
-
-
-    touch(wait(Template(r"tpl1576850905314.png", record_pos=(-0.276, 0.127), resolution=(2520, 1080)))) # 艾蕾3技能
-    touch(wait(Template(r"tpl1576850738968.png", record_pos=(0.075, 0.038), resolution=(2520, 1080))))
-
-    touch(wait(Template(r"tpl1576852106640.png", record_pos=(-0.012, 0.127), resolution=(2520, 1080)))) # 弓凛1技能
-    touch(wait(Template(r"tpl1576850738968.png", record_pos=(0.075, 0.038), resolution=(2520, 1080))))
-
-
-
-    touch(wait(Template(r"tpl1576850638587.png", record_pos=(0.242, 0.142), resolution=(2520, 1080))))
-    touch(wait(Template(r"tpl1576853632199.png", record_pos=(-0.18, -0.056), resolution=(2520, 1080))))
-    sleep()
-    touch(v=(350,900)) # 指令卡1
-    sleep()
-    touch(v=(750,900)) # 指令卡2
-    sleep()
-    snapshot(msg="艾蕾宝具动画")
-
-
-    wait(Template(r"tpl1576850638587.png", record_pos=(0.242, 0.142), resolution=(2520, 1080)),timeout=60) # 等待攻击界面加载完成
-    touch(v=(750,900)) # 小莫1技能
-    touch(wait(Template(r"tpl1576850738968.png", record_pos=(0.075, 0.038), resolution=(2520, 1080))))
-    touch(wait(Template(r"tpl1576851280803.png", record_pos=(-0.088, 0.127), resolution=(2520, 1080)))) # 小莫3技能
-    touch(wait(Template(r"tpl1576850738968.png", record_pos=(0.075, 0.038), resolution=(2520, 1080))))
-
-    touch(wait(Template(r"tpl1576851330274.png", record_pos=(0.099, 0.129), resolution=(2520, 1080)))) # 弓凛3技能
-    touch(wait(Template(r"tpl1576850738968.png", record_pos=(0.075, 0.038), resolution=(2520, 1080))))
-    touch(wait(Template(r"tpl1576850638587.png", record_pos=(0.242, 0.142), resolution=(2520, 1080))))
-
-    touch(wait(Template(r"tpl1576853955975.png", record_pos=(-0.052, -0.052), resolution=(2520, 1080))))
-    sleep()
-    touch(v=(350,900)) # 指令卡1
-    sleep()
-    touch(v=(750,900)) # 指令卡2
-    sleep()
-    snapshot(msg="小莫宝具动画")
-
-    touch(wait(Template(r"tpl1576851569748.png", record_pos=(0.043, 0.129), resolution=(2520, 1080)),timeout=60)) # 弓凛2技能
-    touch(wait(Template(r"tpl1576850738968.png", record_pos=(0.075, 0.038), resolution=(2520, 1080))))
-    touch(wait(Template(r"tpl1576850638587.png", record_pos=(0.242, 0.142), resolution=(2520, 1080))))
-    touch(wait(Template(r"tpl1576854091829.png", record_pos=(0.092, -0.056), resolution=(2520, 1080))))
-    sleep()
-    touch(v=(350,900)) # 指令卡1
-    sleep()
-    touch(v=(750,900)) # 指令卡2
-    sleep()
-    snapshot(msg="弓凛宝具动画")
-
-    touch(wait(Template(r"tpl1576850295381.png", record_pos=(-0.053, 0.168), resolution=(2520, 1080)),timeout=60)) # 从者经验值界面
-    touch(wait(Template(r"tpl1576850295381.png", record_pos=(-0.053, 0.168), resolution=(2520, 1080)))) # 御主经验值界面
-    touch(wait(Template(r"tpl1576850387830.png", record_pos=(0.225, 0.188), resolution=(2520, 1080)))) # QP界面
+for _i in range(8):
+    for _i in range(32):
+        sleep(2.4)
+        touch([450,450])
+    touch(wait(Template(r"tpl1579452354002.png", record_pos=(0.387, -0.088), resolution=(1280, 720))))
+    for _i in range(2):
+        sleep(2.4)
+        touch([750,580])
--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # FGO-AirTester.air
 
-> 此为网易游戏在中山大学计算机科学与技术专业2019~2020学年第一学期开设的选修课程《移动互联网编程实践》作业。
+圣诞四期无限池抽取脚本，单次只抽取八池防止邮箱溢出。
 
-课程报告地址：<https://wu-kan.github.io/posts/%E9%92%88%E5%AF%B9FGO%E7%9A%84%E8%87%AA%E5%8A%A8%E5%8C%96%E6%B5%8B%E8%AF%95%E5%AE%9E%E8%B7%B5>
-
-Airtest脚本开源地址：<https://github.com/wu-kan/FGO-AirTester.air>
-
-Airtest运行报告地址：<https://wu-kan.github.io/FGO-AirTester.air/log.html>
-
-PerfDog性能数据地址：<https://perfdog.qq.com/case_detail/336356>，Password: KBKmyX
-
-完整测试视频地址：<https://www.bilibili.com/video/av80128598/>
+完整测试视频地址：<https://www.bilibili.com/video/av84488554/>
