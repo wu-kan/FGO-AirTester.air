67137275a19d7c9956e545873899b1236faeebdd
wu-kan <wu.kan@qq.com>
2020-01-26 21:47:02 +0800

    Update FGO-AirTester.py

--- FGO-AirTester.py
+++ FGO-AirTester.py
@@ -8,8 +8,12 @@ auto_setup(__file__)
 for _i in range(8):
     for _i in range(32):
         sleep(2.4)
-        touch([450,450])
-    touch(wait(Template(r"tpl1579452354002.png", record_pos=(0.387, -0.088), resolution=(1280, 720))))
+        touch([450, 450])
+    while not exists(Template(r"tpl1579452354002.png", record_pos=(0.387, -0.088), resolution=(1280, 720))):
+        sleep(2.4)
+        touch([450, 450])
+    touch(wait(Template(r"tpl1579452354002.png", record_pos=(
+        0.387, -0.088), resolution=(1280, 720))))
     for _i in range(2):
         sleep(2.4)
-        touch([750,580])
+        touch([750, 580])
