e8811c706ad65365ec0918f8ccc9e941909dac3b
wu-kan <wu.kan@qq.com>
2020-05-19 17:47:59 +0800

    Done！

--- FGO-AirTester.py
+++ FGO-AirTester.py
@@ -6,13 +6,12 @@ from airtest.core.api import *
 auto_setup(__file__)
 
 while(True):
-    for i in range(4):
+    sleep(),touch(wait(Template(r"tpl1589881548920.png", record_pos=(0.109, -0.02), resolution=(1280, 720))))
+    for i in range(1,4):
         for j in range(7):
-            sleep(0.2)
-            touch([j*130+140,i*130+250])
-    sleep()
-    touch(wait(Template(r"tpl1583716275273.png", record_pos=(0.402, 0.241), resolution=(1280, 720))))
-    sleep()
-    touch(wait(Template(r"tpl1583716252169.png", record_pos=(0.155, 0.176), resolution=(1280, 720))))
-    sleep()
-    touch(wait(Template(r"tpl1583716162753.png", record_pos=(0.002, 0.177), resolution=(1280, 720))))
+            sleep(0.2),touch([j*130+140,i*130+250])
+    sleep(),touch(wait(Template(r"tpl1583716275273.png", record_pos=(0.402, 0.241), resolution=(1280, 720))))
+    sleep(),touch(wait(Template(r"tpl1589881387545.png", record_pos=(0.362, 0.248), resolution=(1280, 720))))
+    sleep(),touch(wait(Template(r"tpl1583716275273.png", record_pos=(0.402, 0.241), resolution=(1280, 720))))
+    for i in range(9):
+        sleep(0.2),touch([3*130+140,3*130+250])
